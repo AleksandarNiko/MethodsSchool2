8af917564698d27d67bd1ec12c5890d6d0e78eeb
AleksandarNiko <145496882+AleksandarNiko@users.noreply.github.com>
2023-12-28 20:12:00 +0200

    Add project files.

---
+++ 05/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintLowers();
+        }
+
+         static void PrintLowers()
+        {
+            Console.WriteLine("Write number:");
+            int num = int.Parse(Console.ReadLine());
+            List<int> lows = new List<int>();
+            for (int i = num - 1; i >= 1; i--)
+            {
+                if (i % 5 == 0)
+                {
+                    lows.Add(i);
+                }
+            }
+            Console.WriteLine(string.Join(" ", lows));
+        }
+    }
+    }
+
---
+++ 07/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write numerator:");
+            int num=int.Parse(Console.ReadLine());
+            Console.WriteLine("Write denominator:");
+            int den=int.Parse(Console.ReadLine());
+           
+            int numeratorSum = GetDigitSum(num);
+            int denominatorSum = GetDigitSum(den);
+
+            int numeratorResult = num - numeratorSum;
+            int denominatorResult = den - denominatorSum;
+
+            Console.WriteLine($"{numeratorResult}/{denominatorResult}");
+        }
+
+        static int GetDigitSum(int number)
+        {
+            int sum = 0;
+            while (number > 0)
+            {
+                sum += number % 10;
+                number /= 10;
+            }
+            return sum;
+        }
+    }
+    }
+
